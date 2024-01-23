8e85bcf5c9249d7fa532999236ec0215c3307306
Olubisi Ibrahim Olawale <45831834+ibrotech01@users.noreply.github.com>
2024-01-23 07:26:54 +0100

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,7 +9,6 @@ const propertiesRoute = require("./routes/properties");
 
 const port = 8800;
 dotenv.config();
-
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("DB Connection successfully!"))
 .catch((err) => console.log(err));
@@ -21,7 +20,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/properties", propertiesRoute);
 app.use("/api/agents", agentsRoute);
-
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
